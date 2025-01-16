5016e163d3e67fbebd79b6cebaed74e1f1e760bb
Kostikus <ikonstantins@yahoo.co.uk>
2025-01-16 20:50:28 +0100

    капец еле разобрался

---
+++ lesson_01/homework_01_konstan.py
@@ -0,0 +1,93 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print("helloworld!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+print(banana)
+
+
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
+
+
+"""
+    # Задачі 07 -10:
+    # Переведіть задачі з книги "Математика, 2 клас"
+    # на мову пітон і виведіть відповідь, так, щоб було
+    # зрозуміло дитині, що навчається в другому класі
+"""
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+apples = 4
+pears = 4 + 5
+plums = 4 - 2
+Trees_summ = apples + pears + plums
+print(Trees_summ)
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+mornign_temp = 0 + 5
+afternoon_temp = 5 - 10
+evening_temp = -5 + 4
+print(evening_temp)
+
+
+
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+boys = 24
+girls = boys / 2
+sick_boy = 1
+sick_girl = 2
+present_boys = boys - sick_boy
+present_girls  = girls - sick_girl
+present_kids = present_girls + present_boys
+print(present_boys)
+print(present_girls)
+print(present_kids)
+"""
+
+
+
+# task 10
+"""
+
+
+first_book = 8
+second_book  = first_book + 2
+third_book = (first_book + second_book) / 2
+total_prices = first_book + second_book + third_book
+print(total_prices)
+
