b8f9e32e4730c3d12fa454cb8b023cec25102bcf
Nadiia Protsenko <philimonovanadiya@gmail.com>
2025-01-19 19:14:34 +0200

    Add fixed homework_01

--- lesson_01/homework_01.py
+++ lesson_01/homework_01.py
@@ -1,32 +1,34 @@
 # task 01 == Виправте синтаксичні помилки
 print("Hello", end = " ")
-    print("world!")
+print("world!")
 
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(f"{hello} {world}!")
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
-    print()
+    print(letter)
 
 # task 04 == Зробіть так, щоб кількість бананів була
 # завжди в чотири рази більша, ніж яблук
 apples = 2
+x = apples * 4
 banana = x
+print(banana)
 
 # task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
 
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
 
 
 """
@@ -40,6 +42,12 @@ print()
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+apple_tree = 4
+pear_tree = apple_tree + 5
+plum_tree = apple_tree - 2
+total_tree = apple_tree + pear_tree + plum_tree
+print("Скільки всього дерев посадили в саду?")
+print(f"Відповідь: {total_tree}")
 
 # task 08
 """
@@ -47,6 +55,11 @@ print()
 Після обіду температура опустилася на 10 градусів.
 Надвечір потепліло на 4 градуси. Яка температура надвечір?
 """
+morning_temp = 5
+afternoon_temp = morning_temp - 10
+evening_temp = afternoon_temp + 4
+print("Яка температура надвечір?")
+print(f"Відповідь: {evening_temp}")
 
 # task 09
 """
@@ -54,6 +67,11 @@ print()
 1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
 Скількі сьогодні дітей у театральному гуртку?
 """
+boys = 24
+girls = boys//2
+kids_today = (boys - 1) + (girls - 2)
+print("Скількі сьогодні дітей у театральному гуртку?")
+print(f"Відповідь: {kids_today}")
 
 # task 10
 """
@@ -61,3 +79,9 @@ print()
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
 """
+first_book = 8
+second_book = first_book + 2
+third_book = (first_book + second_book) / 2
+total_price = first_book + second_book + third_book
+print("Скільки будуть коштувати усі книги, якщо купити по одному примірнику?")
+print(f"Відповідь: {total_price}")
