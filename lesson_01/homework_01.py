352a763e94b365b7c9138c6a6f94181cd7b50406
Yuliia <>
2025-01-20 19:31:09 +0200

    Виконані зміни

--- lesson_01/homework_01.py
+++ lesson_01/homework_01.py
@@ -1,32 +1,35 @@
 # task 01 == Виправте синтаксичні помилки
 print("Hello", end = " ")
-    print("world!")
+print("world!")
 
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(f"{hello} {world}!")
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
-    print()
+    print(letter)
 
 # task 04 == Зробіть так, щоб кількість бананів була
 # завжди в чотири рази більша, ніж яблук
 apples = 2
-banana = x
+bananas = apples * 4
+
+print("Number of bananas:", bananas)
 
 # task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+side_1 = 1
+side_2 = 2
+side_3 = 3
+side_4 = 4
 
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimeter = side_1 + side_2 + side_3 + side_4
+
+print("Perimeter of the figure:", perimeter)
 
 
 """
@@ -40,6 +43,13 @@ print()
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+apples_amount = 4
+pears_amount = apples_amount + 5
+plums_amount = apples_amount - 2
+
+trees_amount = apples_amount + pears_amount + plums_amount
+
+print("There are", trees_amount, "trees in the garden")
 
 # task 08
 """
@@ -47,6 +57,11 @@ print()
 Після обіду температура опустилася на 10 градусів.
 Надвечір потепліло на 4 градуси. Яка температура надвечір?
 """
+morning_temperature = 0 + 5
+afternoon_temperature = 5 - 10
+evening_temperature = afternoon_temperature + 4
+
+print("Evening temperature is:", evening_temperature)
 
 # task 09
 """
@@ -54,6 +69,15 @@ print()
 1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
 Скількі сьогодні дітей у театральному гуртку?
 """
+boys_amount = 24
+girls_amount = boys_amount // 2
+
+boys_amount_today = boys_amount - 1
+girls_amount_today = girls_amount - 2
+
+children_amount_today = boys_amount_today + girls_amount_today
+
+print("There are", children_amount_today, "children today on class")
 
 # task 10
 """
@@ -61,3 +85,10 @@ print()
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
 """
+first_book_price = 8
+second_book_price = first_book_price + 2
+third_book_price = (first_book_price + second_book_price) // 2
+
+total_book_price = first_book_price + second_book_price + third_book_price
+
+print(total_book_price, "grivnas is a total price for three books")
