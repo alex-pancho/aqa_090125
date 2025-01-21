8291fb30b8943c87fb7b5e82df25ff34c1fd22b2
Leonid Svyatnyi <leonid.svyatnyi@gmail.com>
2025-01-21 22:23:57 +0200

    -Fixed syntaxis issues for tasks 1 and 2 -Added missed variable 'letter' to function print for taks 3 -changed variable banana for making it 4 times bigger then applease task 4 -fixed names for variables in task 5 -changed variable 'perimetery' to calculculate and shown result for taks 6 -creating code for finding results for tasks 7-10

--- lesson_01/homework_01.py
+++ lesson_01/homework_01.py
@@ -1,32 +1,32 @@
 # task 01 == Виправте синтаксичні помилки
-print("Hello", end = " ")
-    print("world!")
+print("Hello")
+print("world!")
 
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(hello, world)
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
-    print()
+    print(letter)
 
 # task 04 == Зробіть так, щоб кількість бананів була
 # завжди в чотири рази більша, ніж яблук
 apples = 2
-banana = x
+banana = apples * 4
 
 # task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
 
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
 
 
 """
@@ -35,11 +35,17 @@ print()
     # на мову пітон і виведіть відповідь, так, щоб було
     # зрозуміло дитині, що навчається в другому класі
 """
+
 # task 07
 """
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+apples = 4
+pears = apples + 5
+plums = apples - 2
+total_trees = apples + pears +plums
+print("Total number of trees in the gardgen are:", total_trees)
 
 # task 08
 """
@@ -47,6 +53,10 @@ print()
 Після обіду температура опустилася на 10 градусів.
 Надвечір потепліло на 4 градуси. Яка температура надвечір?
 """
+temp_before_lunch = 5
+temp_after_lunch = temp_before_lunch - 10
+temp_evening = temp_after_lunch + 4
+print("Temperature in the evening is:", temp_evening)
 
 # task 09
 """
@@ -54,6 +64,12 @@ print()
 1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
 Скількі сьогодні дітей у театральному гуртку?
 """
+boys = 24
+girls = boys / 2
+sick = 1
+absent = 2
+children_today = boys + girls - sick - absent
+print("Today we have", children_today, "children in theater club ")
 
 # task 10
 """
@@ -61,3 +77,8 @@ print()
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
 """
+price_book_1 = 8
+price_book_2 = price_book_1 + 2
+price_book_3 = (price_book_1 + price_book_2) / 2
+total_price = price_book_1 + price_book_2 + price_book_3
+print("Total price is", total_price, "UAH")
