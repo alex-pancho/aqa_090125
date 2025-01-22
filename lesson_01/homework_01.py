6a590885133e6e75ab0ec3192a0f8529a13352a2
Anton <honcharukanton.sh@gmail.com>
2025-01-23 00:25:47 +0200

    Виконані зміни

--- lesson_01/homework_01.py
+++ lesson_01/homework_01.py
@@ -1,32 +1,32 @@
 # task 01 == Виправте синтаксичні помилки
 print("Hello", end = " ")
-    print("world!")
+print("world!")
 
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
-print(f"{hello} {world}!")
+    print(f"{hello} {world}!")
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
-    print()
+    print(letter)
 
 # task 04 == Зробіть так, щоб кількість бананів була
 # завжди в чотири рази більша, ніж яблук
 apples = 2
-banana = x
+bananas = 4*apples
 
 # task 05 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
 
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimeter = storona_1 + storona_2 + storona_3 + storona_4
+print(perimeter)
 
 
 """
@@ -40,7 +40,12 @@ print()
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+apple_trees = 4
+pear_trees = apple_trees + 5
+plum_trees = apple_trees - 2
+total_trees = apple_trees + pear_trees + plum_trees
 
+print(f" Сумарно висаджено {total_trees} дерев")
 # task 08
 """
 До обіда температура повітря була на 5 градусів вище нуля.
@@ -48,16 +53,31 @@ print()
 Надвечір потепліло на 4 градуси. Яка температура надвечір?
 """
 
-# task 09
-"""
-Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
-1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
-Скількі сьогодні дітей у театральному гуртку?
-"""
+morning_temp = 5
+afternoon_temp = morning_temp - 10
+evening_temp = afternoon_temp + 4
 
+print(f" Вечірня температура {evening_temp} градус")
+# task 09
+
+boys = 24
+girls = boys // 2
+absent_boys = 1
+absent_girls = 2
+present_children = boys + girls - absent_girls - absent_boys
+
+print(f" У театральному гуртку  {present_children} дитини")
 # task 10
 """
-Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+#Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
 """
+
+
+book1_price = 8
+book2_price = book1_price + 2
+book3_price = (book1_price + book2_price) // 2
+total_price = book1_price + book2_price + book3_price
+
+print(f" Сумарна вартість {total_price} грн")
