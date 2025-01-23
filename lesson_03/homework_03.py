535c62f1ad8b2e6655db2a6c484c244366f99b50
Nadiia Protsenko <philimonovanadiya@gmail.com>
2025-01-23 17:55:48 +0200

    Add homework03

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -1,8 +1,17 @@
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
+#alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
+#свариться на символ —— , як правильно треба було це вирішити?
 # task 02 == Знайдіть та екрануйте всі символи одинарної дужки у тексті
+alice_in_wonderland = (
+    '"Would you tell me, please, which way I ought to go from here?"\n'
+    '"That depends a good deal on where you want to get to," said the Cat.\n'
+    '"I don\'t much care where - " said Alice.\n'  # Замінено довге тире на дефіс
+    '"Then it doesn\'t matter which way you go," said the Cat.\n'
+    '"- so long as I get somewhere," Alice added as an explanation.\n'
+    '"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."'
+)
 # task 03 == Виведіть змінну alice_in_wonderland на друк
-
+print(alice_in_wonderland)
 
 """
     # Задачі 04 -10:
@@ -16,7 +25,10 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 моря становить 37 800 км2. Яку площу займають Чорне та Азов-
 ське моря разом?
 """
-
+blackseasquare = 436402
+azovseasquare = 37800
+total_sea_square = blackseasquare + azovseasquare
+print(total_sea_square)
 
 # task 05
 """
@@ -25,7 +37,13 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 250 449 товарів. На другому та третьому – 222 950 товарів.
 Знайдіть кількість товарів, що розміщені на кожному складі.
 """
-
+first_second = 250449
+second_third = 222950
+total_goods = 375291
+first_goods = total_goods - second_third
+second_goods = total_goods - first_second
+third_goods = total_goods - first_goods
+print(first_goods, second_goods, third_goods)
 
 # task 06
 """
@@ -34,7 +52,10 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 вати необхідно буде півтора року по 1179 грн/місяць. Обчисліть
 вартість комп’ютера.
 """
-
+monthly_payment = 1179
+payment_period = 1.5
+total_cost = monthly_payment * payment_period
+print(total_cost)
 
 # task 07
 """
@@ -43,7 +64,13 @@ a) 8019 : 8     d) 7248 : 6
 b) 9907 : 9     e) 7128 : 5
 c) 2789 : 5     f) 19224 : 9
 """
-
+a = 8019 % 8
+b = 9907 % 9
+c = 2789 % 5
+d = 7248 % 6
+e = 7128 % 5
+f = 19224 % 9
+print(a, b, c, d, e, f)
 
 # task 08
 """
@@ -57,7 +84,18 @@ c) 2789 : 5     f) 19224 : 9
 Торт            1           350 грн
 Вода            3           21 грн
 """
-
+pizza_big = 4
+pizza_medium = 2
+juice = 4
+cake = 1
+water = 3
+pizza_big_price = 274
+pizza_medium_price = 218
+juice_price = 35
+cake_price = 350
+water_price = 21
+total_price = pizza_big * pizza_big_price + pizza_medium * pizza_medium_price + juice * juice_price + cake * cake_price + water * water_price
+print(total_price)
 
 # task 09
 """
@@ -66,6 +104,12 @@ c) 2789 : 5     f) 19224 : 9
 розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
 Ігорю, щоб вклеїти всі фото?
 """
+photoes = 232
+photoes_per_page = 8
+pages = photoes // photoes_per_page
+if photoes % photoes_per_page:
+    pages += 1 
+print(pages)
 
 
 # task 10
@@ -79,3 +123,12 @@ c) 2789 : 5     f) 19224 : 9
 равку під час цієї подорожі, кожного разу заправляючи пов-
 ний бак?
 """
+distance = 1600
+fuel_per_100km = 9
+tank_capacity = 48
+fuel_needed = distance / 100 * fuel_per_100km
+refills = fuel_needed // tank_capacity
+if fuel_needed % tank_capacity:
+    refills += 1
+print(fuel_needed, refills)
+
