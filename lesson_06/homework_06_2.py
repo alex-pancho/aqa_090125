46097ee145c76dc5ff6ca229037cae6837fdf440
Nadiia Protsenko <philimonovanadiya@gmail.com>
2025-02-03 19:34:01 +0200

    fix test 'h' and 'H'

--- lesson_06/homework_06_2.py
+++ lesson_06/homework_06_2.py
@@ -2,7 +2,7 @@
 # #в якому є літера "h" (враховуються як великі так і маленькі). 
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
 user_input = input("Enter a word: ")
-while "h" not in user_input or "H" not in user_input:
+while "h" not in user_input and "H" not in user_input:
     user_input = input("Enter a word with the letter 'h' or 'H': ")
 print("The word contains the letter 'h' or 'H'.")
 
