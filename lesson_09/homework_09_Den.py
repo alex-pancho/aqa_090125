f892fc6a1fe2c821906be65ce0d91a3b6f763a4f
Denis-Kovylin <122086120+Denis-Kovylin@users.noreply.github.com>
2025-02-27 14:36:50 +0200

    Homework #9 is done. 3 functions with 10 tests (including negative).

---
+++ lesson_09/homework_09_Den.py
@@ -0,0 +1,64 @@
+
+def count_vowels(input_string: str):
+    """Функция подсчета гласных букв в строке"""
+    vowels_list = ['a', 'e', 'o', 'y', 'i', 'u']
+    vowels_counts = {}
+    for letter in input_string.lower():
+        if letter in vowels_list:
+            if letter in vowels_counts:
+               vowels_counts[letter] += 1
+            else:
+                vowels_counts[letter] = 1
+    return vowels_counts
+print(count_vowels("Hello, world!"))  # {'e': 1, 'o': 2}
+print(count_vowels("Python is awesome!"))  # {'o': 2, 'i': 1, 'a': 1, 'e': 2}
+print(count_vowels("AEIOUYaeiouy"))  # {'a': 2, 'e': 2, 'i': 2, 'o': 2, 'u': 2, 'y': 2}
+print(count_vowels("bcdfg"))  # {}
+
+
+
+
+def compare_grades(grades_1: dict, grades_2: dict):
+    """Функция сравнения оценок студентов"""
+    result = {}
+    all_students = set(grades_1) | set(grades_2)
+    for student in all_students:
+        if student in grades_1 and student in grades_2:
+            result[student] = grades_1[student] - grades_2[student]
+        elif student in grades_1 and student not in grades_2:
+            result[student] = grades_1[student]
+        elif student in grades_2 and student not in grades_1:
+            result[student] = grades_2[student]
+
+    return result
+print(compare_grades(
+    {'Анна': 90, 'Ваня': 89},
+    {'Анна': 85, 'Ваня': 87}
+))
+
+print(compare_grades(
+    {'Анна': 90, 'Ваня': 89},
+    {'Анна': 90}
+))
+
+print(compare_grades(
+    {'Анна': 90},
+    {'Анна': 85, 'Ваня': 87}
+))
+
+
+
+def sum_positive_numbers(numbers: list):
+    """Возвращает сумму всех положительных чисел в списке"""
+    if not isinstance(numbers, list):
+        raise ValueError("Функция принимает в качестве аргумента только список")
+
+    if not all(isinstance(number, (int, float)) for number in numbers):
+        raise ValueError("Список должен содержать исключительно числа")
+    total = sum(number for number in numbers if number > 0)
+
+    return total
+print(sum_positive_numbers([1, -2, 3, 4, -5]))
+print(sum_positive_numbers([-1, -2, -3]))
+print(sum_positive_numbers([10, 20, 30]))
+print(sum_positive_numbers([]))
---
+++ lesson_09/test_homeworks_09_Den.py
@@ -0,0 +1,47 @@
+import unittest
+
+from aqa_local_repo.aqa_090125.lesson_08.homework_08 import sum_numbers_in_list
+from homework_09_Den import count_vowels, compare_grades, sum_positive_numbers
+
+class TestHomework09(unittest.TestCase):
+    """Тесты для подсчета гласных в строке"""
+    def test_string_with_vowels(self):
+        self.assertEqual(count_vowels("Python is awesome!"), {'o': 2, 'i': 1, 'a': 1, 'e': 2, 'y': 1})
+
+    def test_no_vowels_string(self):
+        self.assertEqual(count_vowels("bcdfg"), {})
+
+    def test_insensitivity_to_case(self):
+        self.assertEqual(count_vowels("AEIOUYaeiouy"), {'a': 2, 'e': 2, 'i': 2, 'o': 2, 'u': 2, 'y': 2})
+
+    """Тесты для для сравнения оценок студентов"""
+    def test_routine_positive_check(self):
+        self.assertEqual(compare_grades({'Анна': 90, 'Ваня': 89}, {'Анна': 85, 'Ваня': 87}), {'Анна': 5, 'Ваня': 2})
+
+    def test_student_is_missing_g1(self):
+        self.assertEqual(compare_grades({'Анна': 90},{'Анна': 85, 'Ваня': 87}), {'Ваня': 87, 'Анна': 5})
+
+    def test_student_is_missing_g2(self):
+        self.assertEqual(compare_grades({'Анна': 90, 'Ваня': 89},{'Анна': 90}), {'Анна': 0, 'Ваня': 89})
+
+    """Тесты для суммирования чисел в списке"""
+    def test_positive_numbers(self):
+        self.assertEqual(sum_positive_numbers([1, 2, 3, 4]), 10)
+
+    def test_positive_and_negative_numbers(self):
+        self.assertEqual(sum_positive_numbers([0, -1, 5, -2, 10]), 15)
+
+    def test_empty_list(self):
+        self.assertEqual(sum_positive_numbers([]), 0)
+
+    """Негативные проверки"""
+    def test_invalide_type(self):
+        with self.assertRaises(ValueError):
+            sum_positive_numbers("123")
+        with self.assertRaises(ValueError):
+            sum_positive_numbers(123)
+        with self.assertRaises(ValueError):
+            sum_positive_numbers({'a': 1})
+
+if __name__ == "__main__":
+    unittest.main
