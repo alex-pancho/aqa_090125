cd9f010e0cdc5b19492184da9e4b33a06baa6c14
Viktoriia Lesyk <viktoriiales@gmail.com>
2025-03-04 20:55:08 +0200

    Homework 14

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -30,3 +30,55 @@ else:
 
 Написати на це все тести
 """
+
+class SiteUser():
+   
+    def __init__(self, name:str, email:str, level_access, logcount = 0):
+        self.name = name
+        self.__email = email
+        self.__level_access = level_access
+        self.logcount = logcount
+
+    @property  
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, email:str):
+            self.__email = email
+        
+    @property  
+    def level_access(self):
+        return self.__level_access
+    
+    @level_access.setter
+    def level_access(self, level_access:str):
+            self.__level_access = level_access    
+
+    def track_logcount(self):
+        self.logcount += 1 
+
+    def __str__(self):
+        return f"SiteUser: {self.name}, mailbox: {self.__email}, access level: {self.__level_access}, logcount: {self.logcount}"
+    
+    def __eq__(self, other):
+        if isinstance(other, SiteUser):
+            return self.__level_access == other.__level_access
+        return False
+         
+user1 = SiteUser("John Doe", "john.doe@example.com", "user")
+user2 = SiteUser("Jane Smith", "jane.smith@example.com", "admin")
+
+print(user1)
+# Виведе: SiteUser: John Doe, mailbox: john.doe@example.com, access level: user
+
+# Порівняння користувачів
+if user1 == user2:
+    print("Користувачі однакові")
+else:
+    print("Користувачі різні")
+
+user2.track_logcount()
+print(user2)
+user2.track_logcount()
+print(user2)
--- lesson_14/homework_14_lms.py
+++ lesson_14/homework_14_lms.py
@@ -4,3 +4,24 @@
 Потім додайте метод до класу "Студент", який дозволяє змінювати середній бал студента.
 Виведіть інформацію про студента та змініть його середній бал.
 """
+
+class Student():
+    def __init__(self, first_name:str, last_name:str, age:int, avg_score:float):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.avg_score = avg_score
+    
+    def update_average_score(self, value):
+        self.avg_score = value
+
+rose_student = Student("Rose", "Tyler", 19, 4.8)
+
+print("First Name:", rose_student.first_name)
+print("Last Name:", rose_student.last_name)
+print("Age:", rose_student.age)
+print("Average sccore before update:", rose_student.avg_score)
+
+rose_student.update_average_score(4.3)
+
+print("Average sccore after update:", rose_student.avg_score)
