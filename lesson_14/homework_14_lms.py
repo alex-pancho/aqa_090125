bf5ff68899ab566c57de3fc7c07f39cefabcb84f
Sergey Babich <sergey@github.repo.com>
2025-03-05 11:30:44 +0200

    HW14 minor updates to lms homework

--- lesson_14/homework_14_lms.py
+++ lesson_14/homework_14_lms.py
@@ -5,7 +5,7 @@
 Виведіть інформацію про студента та змініть його середній бал.
 """
 class Student:
-    def __init__(self, *, first_name, last_name, avg_score):
+    def __init__(self, *, first_name:str, last_name:str, avg_score:float):
         self.first_name = first_name
         self.last_name = last_name
         self.__avg_score = avg_score
@@ -15,13 +15,15 @@ class Student:
 
     @property
     def avg_score(self):
-        return self.__avg_score
+        return '{:.1f}'.format(self.__avg_score)
 
     @avg_score.setter
     def avg_score(self, value:float):
-        self.__avg_score = value
+        if isinstance(value, float | int) and value >= 0:
+            self.__avg_score = value
 
-student1 = Student(first_name='Sergey', last_name='Babych', avg_score=99.9)
+student1 = Student(first_name='Sergey', last_name='Babych', avg_score=99.5)
 print(student1)
-student1.avg_score = 99
-print(student1)
+print(student1.avg_score)
+student1.avg_score = 1
+print(student1.avg_score)
