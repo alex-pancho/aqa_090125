ae46f2393d01133834c80e3886db96caee77c1ce
olhasyn <olhastests@ukr.net>
2025-03-12 11:04:21 +0200

    File hw_15 was updated by Olha Synelnykova

--- lesson_15/hw_15.py
+++ lesson_15/hw_15.py
@@ -13,3 +13,31 @@
     кут_б обчислюється автоматично.
     Для встановлення значень атрибутів використовуйте метод __setattr__.
 """
+
+class Rhombus:
+    side_a = None
+    angle_a = None
+    angle_b = None
+    angle_d = None # додала кут, який знаходиться між сторонами 'a' та 'd' (тупий кут)
+
+    def __setattr__(self, name, value):
+        if name == "side_a":
+            if value <= 0:
+                raise ValueError("Значення сторони сторони 'a' повинно бути більше 0 !")
+            object.__setattr__(self, name, value)
+        elif name == "angle_a":
+            object.__setattr__(self, name, value)
+            object.__setattr__(self, "angle_b", 180 - value)
+            object.__setattr__(self, "angle_d", (360 - value*2)/2) # користуючись твердженням, що протилежні кути ромба рівні
+        elif name == "angle_b":
+            raise AttributeError("Значення кута 'b' вираховується автоматично")
+        else:
+            object.__setattr__(self, name, value)
+
+    def __str__(self):
+        return f'Side a = {self.side_a}, angle a = {self.angle_a}, angle b = {self.angle_b}, angle d = {self.angle_d}'
+   
+romb = Rhombus()
+romb.side_a = 8
+romb.angle_a = 60
+print(romb) # Side a = 8, angle a = 50, angle b = 130, angle d = 130.0
