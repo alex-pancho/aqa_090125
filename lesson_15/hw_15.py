9f6ea30da5247aa64f25c7c05711adf84be03a64
olhasyn <olhastests@ukr.net>
2025-03-14 15:08:48 +0200

    Changes have been made to file hw_15 according to comments

--- lesson_15/hw_15.py
+++ lesson_15/hw_15.py
@@ -15,10 +15,11 @@
 """
 
 class Rhombus:
-    side_a = None
-    angle_a = None
-    angle_b = None
-    angle_d = None # додала кут, який знаходиться між сторонами 'a' та 'd' (тупий кут)
+    def __init__(self, side_a=None, angle_a=None):
+        self.side_a = side_a
+        self.angle_a = angle_a
+        self.angle_b = 180 - angle_a
+        self.angle_d = (360 - angle_a*2)/2
 
     def __setattr__(self, name, value):
         if name == "side_a":
@@ -26,18 +27,14 @@ class Rhombus:
                 raise ValueError("Значення сторони сторони 'a' повинно бути більше 0 !")
             object.__setattr__(self, name, value)
         elif name == "angle_a":
+            if not (0 < value < 180):
+                raise ValueError("Значення кута має бути в діапазоні 180 градусів !")
             object.__setattr__(self, name, value)
-            object.__setattr__(self, "angle_b", 180 - value)
-            object.__setattr__(self, "angle_d", (360 - value*2)/2) # користуючись твердженням, що протилежні кути ромба рівні
-        elif name == "angle_b":
-            raise AttributeError("Значення кута 'b' вираховується автоматично")
         else:
             object.__setattr__(self, name, value)
 
     def __str__(self):
         return f'Side a = {self.side_a}, angle a = {self.angle_a}, angle b = {self.angle_b}, angle d = {self.angle_d}'
    
-romb = Rhombus()
-romb.side_a = 8
-romb.angle_a = 60
+romb = Rhombus(8,60)
 print(romb) # Side a = 8, angle a = 50, angle b = 130, angle d = 130.0
