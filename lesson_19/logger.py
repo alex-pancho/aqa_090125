9bb4be1de1797b8cbcb322613d323b3170ded756
Bulii <dmitriy.buliy@fyst.com>
2025-03-29 09:29:40 +0200

    [homework_19] - Work with requests

---
+++ lesson_19/logger.py
@@ -0,0 +1,10 @@
+
+import logging
+
+logging.basicConfig(
+    handlers=[logging.StreamHandler()],
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
