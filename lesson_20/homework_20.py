90b35e8aa531e9801893b5a7b8232376a3d18994
Bulii <dmitriy.buliy@fyst.com>
2025-04-04 15:38:51 +0300

    [homework_20] - Work with sqlite

--- lesson_20/homework_20.py
+++ lesson_20/homework_20.py
@@ -13,3 +13,87 @@
     Здати ЯК ПР. 
     Файл бази в ПР не включати.
 """
+
+
+import sqlite3
+from pathlib import Path
+
+DB_PATH = Path("market.db")
+
+PRODUCTS_DATA = [("Salo", "100", "Made in Ukraine"), ("Cola", "100", "Made in US"), ("Beer", "100", "Made in Czech")]
+CATEGORIES_DATA = [(1, "Food", "Much salt"), (2, "Drink", "Very sweet"), (3, "Alcohol", "Get relax")]
+
+
+def create_product_table(cursor):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY,
+            product_name TEXT,
+            product_price TEXT,
+            product_description TEXT
+            )
+    ''')
+
+def create_category_table(cursor):
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS categories (
+            id INTEGER PRIMARY KEY,
+            category_id INTEGER,
+            category_name TEXT,
+            category_description TEXT
+            )
+    ''')
+
+def insert_products(cursor, product_name, product_price, product_description):
+    cursor.execute('''
+        INSERT INTO products (product_name, product_price, product_description) VALUES (?, ?, ?)
+    ''', (product_name, product_price, product_description))
+
+def insert_category(cursor, category_id, category_name, category_description):
+    cursor.execute('''
+        INSERT INTO categories (category_id, category_name, category_description) VALUES (?, ?, ?)
+    ''', (category_id, category_name, category_description))
+
+
+def get_products_info(cursor):
+    all_data = cursor.execute(
+        '''SELECT product_name, product_price, product_description, category_name, category_description
+         from products JOIN categories ON products.id == categories.category_id
+         ''')
+    return all_data.fetchall()
+
+
+def get_products(db_path):
+
+    with ConnectDB(db_path) as conn:
+        cursor = conn.cursor()
+
+        create_product_table(cursor)
+        for i in PRODUCTS_DATA:
+            insert_products(cursor, *i)
+
+        create_category_table(cursor)
+        for i in CATEGORIES_DATA:
+            insert_category(cursor, *i)
+
+        return get_products_info(cursor)
+
+
+class ConnectDB:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    print(get_products(DB_PATH))
