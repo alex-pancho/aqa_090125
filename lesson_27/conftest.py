6492adcd10a251c534d8f9e2a533d93d25cd3253
Viktoriia Lesyk <viktoriiales@gmail.com>
2025-05-05 00:38:23 +0300

    Homevork 27

--- lesson_27/conftest.py
+++ lesson_27/conftest.py
@@ -1,8 +1,15 @@
 import pytest
 from get_browser import firefox, chrome
+from selenium.webdriver import Firefox
 
 @pytest.fixture
 def browser():
     _driver = firefox()
     yield _driver
     _driver.quit()
+
+@pytest.fixture
+def firefox_browser():
+    driver = Firefox()
+    yield driver
+    driver.quit()
