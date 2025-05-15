fcfd02ff4aa8a5bea032b9d9db1971472488cd7e
Fofina <fofina18@gmail.com>
2025-05-15 14:24:05 +0300

    homework_30 is done, Fofina

--- lesson_28/homework_28/tests/test_registration.py
+++ lesson_28/homework_28/tests/test_registration.py
@@ -1,27 +1,37 @@
 from faker import Faker
+import allure
 
 faker = Faker()
 
 
 class TestRegistration:
 
+    @allure.title("Test user registration")
+    @allure.description("Test that the user was able to successfully registered")
+    @allure.severity(allure.severity_level.MINOR)
     def test_user_registration(
             self, chrome_browser, open_page, open_registration_popup, fill_registration_popup,
             send_registration_popup, check_switch_to_garage_page
     ):
-        open_page()
-        open_registration_popup()
+        with allure.step("Open page and registration popup"):
+            open_page()
+
+        with allure.step("Open registration popup"):  
+            open_registration_popup()
 
         passowrd = faker.password()
 
-        fill_registration_popup(
-            name=faker.last_name(),
-            last_name=faker.last_name(), 
-            email=faker.email(),
-            password=passowrd,
-            repeat_password=passowrd,
-        )
+        with allure.step("Fill registration popup"):
+            fill_registration_popup(
+                name=faker.last_name(),
+                last_name=faker.last_name(), 
+                email=faker.email(),
+                password=passowrd,
+                repeat_password=passowrd,
+            )
 
-        send_registration_popup()
-        check_switch_to_garage_page()
+        with allure.step("Send registration popup"):
+            send_registration_popup()
+        with allure.step("Check switch to garage page"):    
+            check_switch_to_garage_page()
         
